25ce6d7de227ef1db5031a048c4c97e6a5acaca1
Developer <Developer@test.com>
2014-05-19 23:13:14 -0700

    Sample Findbug demo

---
+++ src/main/java/com/picsauditing/action/SampleError.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.picsauditing.action;
+
+/**
+ * @author developer
+ *
+ */
+public class SampleError {
+	
+	public void testFindbug(){
+		String aString = "bob";
+		aString.replace('b', 'p');
+		if(aString.equals("pop"));
+	}
+	
+}
